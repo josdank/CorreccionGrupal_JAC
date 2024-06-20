c0faea4727f5af6db00ec058127d098faae7ca58
Adrian Ramos <luchoramos042004@gmail.com>
2024-06-20 07:37:01 -0500

    xd

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.ClientInfoStatus;
+import java.util.Scanner;
+import BusesBuho.cliente;
+import BusesBuho.ruta;
+import BusesBuho.ticket;
+import BusesBuho.Servicios;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        cliente pasajero1 = new cliente();
+        ruta r1 = new ruta(pasajero1);
+        ticket t1 = new ticket(pasajero1);
+        Servicios s1 = new Servicios(pasajero1);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        pasajero1.setNombre("Adrian Ramos");
+
+        r1.seleccionDeRuta();
+        t1.seleccionCategoria();
+        t1.precioTicket();
+        s1.selecciondeServicio();
+
+        pasajero1.mostrarDatosCliente();
     }
 }
