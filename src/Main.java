ad980a8905d4b36e4997908c506dd91e0d411a27
Carlos Perez <carlos.perez02@epn.edu.ec>
2024-06-20 07:43:16 -0500

    Commit nuevos Pasajeros

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import BusesBuho.Servicios;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Adrian Ramos
         cliente pasajero1 = new cliente();
         ruta r1 = new ruta(pasajero1);
         ticket t1 = new ticket(pasajero1);
@@ -20,5 +22,36 @@ public class Main {
         s1.selecciondeServicio();
 
         pasajero1.mostrarDatosCliente();
+
+        // Josue Guerra
+        cliente pasajero2 = new cliente();
+        ruta r2 = new ruta(pasajero2);
+        ticket t2 = new ticket(pasajero2);
+        Servicios s2 = new Servicios(pasajero2);
+
+        pasajero2.setNombre("Josue Guerra");
+
+        r2.seleccionDeRuta();
+        t2.seleccionCategoria();
+        t2.precioTicket();
+        s2.selecciondeServicio();
+
+        pasajero2.mostrarDatosCliente();
+
+        // Carlos Perez
+
+        cliente pasajero3 = new cliente();
+        ruta r3 = new ruta(pasajero3);
+        ticket t3 = new ticket(pasajero3);
+        Servicios s3 = new Servicios(pasajero3);
+
+        pasajero3.setNombre("Carlos Perez");
+
+        r3.seleccionDeRuta();
+        t3.seleccionCategoria();
+        t3.precioTicket();
+        s3.selecciondeServicio();
+
+        pasajero3.mostrarDatosCliente();
     }
 }
